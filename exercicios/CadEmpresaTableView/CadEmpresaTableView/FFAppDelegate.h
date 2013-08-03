7a4332a4472d772c6389759f95a245e44d2b696a
Flávio Júnior <flaviojuniorsb@gmail.com>
2013-08-03 11:39:49 -0300

    Exercício feito na aula da 03/08/2013.

---
+++ exercicios/CadEmpresaTableView/CadEmpresaTableView/FFAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FFAppDelegate.h
+//  CadEmpresaTableView
+//
+//  Created by Flávio Júnior on 03/08/13.
+//  Copyright (c) 2013 FF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FFViewController;
+
+@interface FFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FFViewController *viewController;
+
+@end
---
+++ exercicios/CadEmpresaTableView/CadEmpresaTableView/FFViewController.h
@@ -0,0 +1,13 @@
+//
+//  FFViewController.h
+//  CadEmpresaTableView
+//
+//  Created by Flávio Júnior on 03/08/13.
+//  Copyright (c) 2013 FF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FFViewController : UIViewController
+
+@end
