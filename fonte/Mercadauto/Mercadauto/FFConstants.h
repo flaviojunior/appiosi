f2d87d807f3bcaa5a91da9685a681f03d92cd248
Fábio Zoroastro <fabiozoroastro@gmail.com>
2013-08-06 22:30:02 -0300

    Implementacao para utilizar arquivo de constantes e tabela.plist corretamente.

---
+++ fonte/Mercadauto/Mercadauto/FFConstants.h
@@ -0,0 +1,13 @@
+//
+//  FFConstants.h
+//  Mercadauto
+//
+//  Created by fabiozoroastro on 06/08/13.
+//  Copyright (c) 2013 FF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFConstants : NSObject
+
+@end
---
+++ fonte/Mercadauto/Mercadauto/FFConstants.m
@@ -0,0 +1,13 @@
+//
+//  FFConstants.m
+//  Mercadauto
+//
+//  Created by fabiozoroastro on 06/08/13.
+//  Copyright (c) 2013 FF. All rights reserved.
+//
+
+#import "FFConstants.h"
+
+@implementation FFConstants
+
+@end
