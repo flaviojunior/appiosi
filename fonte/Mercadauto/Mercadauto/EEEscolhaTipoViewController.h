3a5b860af03309faf61d6287fc039e57083991c4
Fábio Marinho <fabiomarinho@gmail.com>
2013-11-23 10:50:40 -0200

    Criação da navegação

---
+++ fonte/Mercadauto/Mercadauto/EEEscolhaTipoViewController.h
@@ -0,0 +1,13 @@
+//
+//  EEEscolhaTipoViewController.h
+//  Mercadauto
+//
+//  Created by Fabio Marinho on 23/11/13.
+//  Copyright (c) 2013 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EEEscolhaTipoViewController : UIViewController
+
+@end
---
+++ fonte/Mercadauto/Mercadauto/EEEscolhaTipoViewController.m
@@ -0,0 +1,38 @@
+//
+//  EEEscolhaTipoViewController.m
+//  Mercadauto
+//
+//  Created by Fabio Marinho on 23/11/13.
+//  Copyright (c) 2013 ios. All rights reserved.
+//
+
+#import "EEEscolhaTipoViewController.h"
+
+@interface EEEscolhaTipoViewController ()
+
+@end
+
+@implementation EEEscolhaTipoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
