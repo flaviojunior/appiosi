4e56ab7095baa48f99afa1bc0450a2456d11556b
Fábio Marinho <fabiomarinho@gmail.com>
2013-11-26 20:29:02 -0200

    Inclusão de CorePlot para gráficos

---
+++ fonte/Mercadauto/Mercadauto/EEViewGraficoController.h
@@ -0,0 +1,19 @@
+//
+//  EEViewGraficoController.h
+//  Mercadauto
+//
+//  Created by Fabio Marinho on 26/11/13.
+//  Copyright (c) 2013 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CorePlot-CocoaTouch.h"
+
+@interface EEViewGraficoController : UIViewController<CPTPlotDataSource, CPTAxisDelegate>{
+    CPTXYGraph *graph;
+    NSMutableArray *dataForPlot;
+}
+
+@property (readwrite, strong, nonatomic) NSMutableArray *dataForPlot;
+
+@end
