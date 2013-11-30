f8bb09f61c876aeb7dca2aab7d595b74e3e2c42a
PedroFariasBarbosa <pedrofariasbarbosa@MacBook-Pro-de-Pedro.local>
2013-11-30 13:27:52 -0200

    Ajuste ViewController Selecionar Auto e Table Select Item
    Ajuste ViewController Selecionar Auto e Table Select Item

---
+++ fonte/Mercadauto/Mercadauto/EETableSelectItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  EETableSelectItemViewController.h
+//  Mercadauto
+//
+//  Created by Pedro Farias Barbosa on 30/11/13.
+//  Copyright (c) 2013 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EETableSelectItemViewController : UITableViewController
+
+@end
